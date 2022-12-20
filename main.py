0741008b3fc492719055e69e4a38e620fe95726d
Anže Oblak <anze.oblak@rettro.si>
2022-12-20 16:26:42 +0100

    Added full adder

--- main.py
+++ main.py
@@ -26,6 +26,21 @@ def half_adder(T, state, *params):
     return a, b, c_out, s_out
 
 
+def full_adder(T, state, *params):
+    alpha, Kd, n, delta = params
+    par = alpha, Kd, n
+    a, b, c, s, carry = state
+
+    a_xor_b = XOR(a, b, par)  # a XOR b
+    s_out = XOR(a_xor_b, c, par) + degrade(s, delta)  # (a XOR b) XOR c
+
+    c_and_a_xor_b = AND(c, a_xor_b, par)  # c AND (a XOR b)
+    a_and_b = AND(a, b, par)  # a AND b
+    carry_out = OR(c_and_a_xor_b, a_and_b, par) + degrade(carry, delta)  # (a AND b) OR (c AND (a XOR b))
+
+    return a, b, c, s_out, carry_out
+
+
 if __name__ == "__main__":
     params = alpha, Kd, n, delta
     t_end = 100
@@ -38,7 +53,7 @@ if __name__ == "__main__":
               (0, 10),
               (10, 0),
               (10, 10)]
-    # Inputs for majority
+    # Inputs for majority and full adder
     inputs = [
         (0, 0, 0),
         (0, 0, 10),
@@ -80,6 +95,14 @@ if __name__ == "__main__":
         ax.set_xlabel('Time')
         ax.set_ylabel('Concentrations')
         ax.set_title(f"A={ins[0]}, B={ins[1]}")"""
+
+        """
+        # For full adder
+        ax.plot(T, z.T[:, 3:])
+        ax.legend(['Sum', 'Carry'])
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Concentrations')
+        ax.set_title(f"A={ins[0]}, B={ins[1]}, Cin={ins[2]}")"""
 
     f.set_size_inches(10, 10)
     f.tight_layout()
