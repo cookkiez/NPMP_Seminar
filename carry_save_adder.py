98cd5c2264c9d3b2132261531037bb7870acbaf5
Ambrož <aj8977@student.uni-lj.si>
2023-01-01 20:28:23 +0100

    Summation works, carry not so much. Current input case is incorrect.

--- carry_save_adder.py
+++ carry_save_adder.py
@@ -18,15 +18,17 @@ def chunks(lst, n):
 def carry_save_adder(T, state, *params):
     alpha, Kd, n, delta = params
     par = alpha, Kd, n, delta
-    x, y, z, carry_out, sum_out, prev_carry = chunks(state, n_bits)
+    x, y, z, c, s, prev_carry = chunks(state, n_bits)
+    carry_out = np.zeros(len(c))
+    sum_out = np.zeros(len(s))
     carry_out[0] = prev_carry
     for i in range(n_bits):
         state_i = [int(x[i]), int(y[i]), int(z[i]), 0, 0]
-        sum_degrade = degrade(sum_out[i], delta)
+        sum_degrade = degrade(s[i], delta)
         x[i], y[i], z[i], carry_i, sum_out[i] = full_adder(T, state_i, alpha, Kd, n, delta) 
         sum_out[i] += sum_degrade
         if i < n_bits - 1: 
-            carry_out[i + 1] = carry_i + degrade(carry_out[i + 1], delta)
+            carry_out[i + 1] = carry_i + degrade(c[i + 1], delta)
         else:
             prev_carry = carry_i + degrade(prev_carry, delta)
         
@@ -46,15 +48,15 @@ if __name__ == "__main__":
         (            
             10, 10, 0, # A
             0, 0, 10,  # B
-            0, 0, 0,   # C
+            10, 10, 0,   # C
             0, 0, 0,   # Carry out
             0, 0, 0,   # Sum out
             0,         # Previous carry value
         )
     ] 
 
-    f, axs = plt.subplots(4, 2, sharey=True)
-    for ax, ins in zip(axs.flat, inputs):
+    f, axs = plt.subplots(n_bits, 2, sharey=True)
+    for ins in inputs:
         # set initial conditions
         Y0 = np.zeros(len(inputs[0]))
 
@@ -67,14 +69,27 @@ if __name__ == "__main__":
         z = sol.sol(T)
 
         sum_ix = 4 * n_bits
+        carry_ix = 3 * n_bits
         num_plots = sum_ix + n_bits
         while sum_ix < num_plots:
+            bit_i = num_plots - sum_ix - 1
+            ax = axs.flat[bit_i]
             ax.plot(T, z.T[:, sum_ix:sum_ix + 1])
-            ax.legend([f"Sum bit {num_plots - sum_ix} concentration"])
+            ax.legend([f"Sum bit {bit_i} concentration"])
             ax.set_xlabel('Time') 
             ax.set_ylabel('Concentrations')
-            ax.set_title(f"Sum bit index: {num_plots - sum_ix}")
+            ax.set_title(f"Sum bit index: {bit_i}")
+
+            bit_i = num_plots - carry_ix - 1
+            ax = axs.flat[bit_i]
+            bit_i -= n_bits
+            ax.plot(T, z.T[:, carry_ix:carry_ix + 1])
+            ax.legend([f"Carry bit {bit_i} concentration"])
+            ax.set_xlabel('Time') 
+            ax.set_ylabel('Concentrations')
+            ax.set_title(f"Carry bit index: {bit_i}")
             sum_ix += 1
+            carry_ix += 1
 
     f.set_size_inches(10, 10)
     f.tight_layout()
