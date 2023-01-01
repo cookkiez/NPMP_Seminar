67e93ec6c464a0e9b698d121465b9f953c0996e7
Ambrož <aj8977@student.uni-lj.si>
2023-01-01 20:10:25 +0100

    Testing CSA. Still not working

--- carry_save_adder.py
+++ carry_save_adder.py
@@ -22,13 +22,15 @@ def carry_save_adder(T, state, *params):
     carry_out[0] = prev_carry
     for i in range(n_bits):
         state_i = [int(x[i]), int(y[i]), int(z[i]), 0, 0]
+        sum_degrade = degrade(sum_out[i], delta)
         x[i], y[i], z[i], carry_i, sum_out[i] = full_adder(T, state_i, alpha, Kd, n, delta) 
-        if i < n_bits - 1:
-            carry_out[i + 1] = carry_i
+        sum_out[i] += sum_degrade
+        if i < n_bits - 1: 
+            carry_out[i + 1] = carry_i + degrade(carry_out[i + 1], delta)
         else:
-            prev_carry = carry_i
+            prev_carry = carry_i + degrade(prev_carry, delta)
         
-    to_return = np.concatenate((x, y, z, carry_out, sum_out, [prev_carry]))
+    to_return = np.concatenate((x, y, z, carry_out, sum_out, prev_carry))
     return to_return
 
 
@@ -45,13 +47,12 @@ if __name__ == "__main__":
             10, 10, 0, # A
             0, 0, 10,  # B
             0, 0, 0,   # C
-            0, 0, 0,    # Sum out
             0, 0, 0,   # Carry out
+            0, 0, 0,   # Sum out
             0,         # Previous carry value
         )
     ] 
 
-    # For full adder
     f, axs = plt.subplots(4, 2, sharey=True)
     for ax, ins in zip(axs.flat, inputs):
         # set initial conditions
@@ -65,12 +66,15 @@ if __name__ == "__main__":
         sol = solve_ivp(carry_save_adder, [0, t_end], Y0, args=params, dense_output=True)
         z = sol.sol(T)
 
-        # For full adder
-        ax.plot(T, z.T[:, 3:])
-        ax.legend(['Carry', 'Sum'])
-        ax.set_xlabel('Time')
-        ax.set_ylabel('Concentrations')
-        ax.set_title(f"A={ins[0]}, B={ins[1]}, Cin={ins[2]}")
+        sum_ix = 4 * n_bits
+        num_plots = sum_ix + n_bits
+        while sum_ix < num_plots:
+            ax.plot(T, z.T[:, sum_ix:sum_ix + 1])
+            ax.legend([f"Sum bit {num_plots - sum_ix} concentration"])
+            ax.set_xlabel('Time') 
+            ax.set_ylabel('Concentrations')
+            ax.set_title(f"Sum bit index: {num_plots - sum_ix}")
+            sum_ix += 1
 
     f.set_size_inches(10, 10)
     f.tight_layout()
