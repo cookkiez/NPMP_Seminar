60c77230b8e436d1b05f3b70e5eb78a4c59484f0
Ambrož <aj8977@student.uni-lj.si>
2023-01-09 20:56:43 +0100

    Display heatmap for multiplication

--- multiplication.py
+++ multiplication.py
@@ -1,35 +1,32 @@
 from gates import *
-from full_adder import *
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
+import itertools
+from carry_save_adder import *
+import plotly.express as px
+import seaborn as sns
 
 
-n_bits = 6
+def binatodeci(binary):
+    bin = [1 if b > 5 else 0 for b in binary]
+    return sum(val*(2**idx) for idx, val in enumerate(reversed(bin)))
 
 
-def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
-
-
-def carry_save_adder(T, state, *params):
-    alpha, Kd, n, delta = params
-    x, y, z, s, c, prev_carry = chunks(state, n_bits)
-    carry_out = np.zeros(len(c) + len(prev_carry))
-    sum_out = np.zeros(len(s))
-    carry_out[0] = prev_carry
-    for i in range(n_bits):
-        state_i = [int(x[i]), int(y[i]), int(z[i]), 0, 0]
-        sum_degrade = degrade(s[i], delta)
-        x[i], y[i], z[i], carry_i, sum_out[i] = full_adder(
-            T, state_i, alpha, Kd, n, delta)
-        sum_out[i] += sum_degrade
-        carry_out[i] = carry_i + degrade(c[i], delta)
-
-    to_return = np.concatenate((x, y, z, sum_out, carry_out))
-    return to_return
+def multiply(x, y):
+    # sestavi vse vrstice, ki jih bo potrebno sesteti
+    max_len = len(x) + len(y)
+    seznam = []
+    odmik = 0
+    while len(y) > 0:
+        element = y.pop()
+        if element == 10:
+            vrstica = [0] * (max_len - odmik - len(x)) + x + [0] * odmik
+        else:
+            vrstica = [0] * max_len
+        seznam.append(vrstica)
+        odmik += 1
+    return seznam
 
 
 def do_plots(ix, ax_ix, num_plots, legend_string, title_string):
@@ -44,44 +41,41 @@ def do_plots(ix, ax_ix, num_plots, legend_string, title_string):
         ix += 1
         ax_ix += 1
 
-def multiply(x, y):
-    # sestavi vse vrstice, ki jih bo potrebno sesteti
-    max_len = len(x) + len(y) - 1
-    seznam = []
-    odmik = 0
-    while len(y) > 0:
-        element = y.pop()
-        if element == 10:
-            vrstica = [0] * (max_len - odmik - len(x)) + x + [0] * odmik
-        else:
-            vrstica = [0] * max_len
-        seznam.append(vrstica)
-        odmik += 1
-    return seznam
+
+def append_to_list(app_list, start_ix, end_ix, z, temp_list):
+    while start_ix < end_ix:
+        temp_list.append(z.T[:, start_ix:start_ix + 1][999][0])
+        start_ix += 1
+    app_list.append(binatodeci(temp_list))
+    return binatodeci(temp_list)
+
 
 if __name__ == "__main__":
-    params = alpha, Kd, n, delta
+    n_bits = 6
+    params = alpha, Kd, n, delta, n_bits
     t_end = 100
     # set simulation parameters
     N = t_end * 10  # number of samples
     T = np.linspace(0, t_end, N)
+    combs = list(itertools.product([0, 10], repeat=int(n_bits / 2), ))
+    combs_list = [list(c) for c in combs]
+    inputs = [ [cc, c] for cc in combs_list for c in combs_list ]
 
-    # pridobim seznam stevil, ki jih moram sesteti
-    seznam = multiply([10,0,10], [0, 10, 0])
+    sum_results = []
+    carry_results = []
 
-    parameters = [0] + seznam[0] + [0] + seznam[1] + [0] + seznam[2] + [0] * n_bits + [0] * n_bits + [0]
-    # Inputs for Carry save adder
-    inputs = [
-        tuple(parameters)
-    ]
+    results = []
 
-    f, axs = plt.subplots(n_bits + 1, 2, sharey=True)
-    for ins in inputs:
+    for curr_in in inputs:
         # set initial conditions
-        Y0 = np.zeros(len(inputs[0]))
+        ins = multiply(curr_in[0].copy(), curr_in[1].copy())
+
+        params = alpha, Kd, n, delta, n_bits
+        parameters = ins[0] + ins[1] + ins[2] + [0] * n_bits + [0] * n_bits + [0]
+        Y0 = np.zeros(len(parameters))
 
         # Set inputs for current iteration
-        for ix, i in enumerate(ins):
+        for ix, i in enumerate(parameters):
             Y0[ix] = i
 
         # solving the initial value problems with scipy
@@ -90,27 +84,16 @@ if __name__ == "__main__":
         z = sol.sol(T)
 
         sum_ix = 3 * n_bits
-        ax_ix = 0
-        do_plots(sum_ix, ax_ix, sum_ix + n_bits,
-                 lambda i: f"Sum bit {i} concentration",
-                 lambda i: f"Sum bit index {i}")
-
+        num_res = sum_ix + n_bits
         carry_ix = 4 * n_bits
-        num_plots = len(inputs[0])
-        ax_ix = n_bits
-        do_plots(carry_ix, ax_ix, num_plots,
-                 lambda i: f"Carry bit {i} concentration",
-                 lambda i: f"Carry bit index: {i}")
 
-    f.set_size_inches(10, 10)
-    f.tight_layout()
-    plt.tight_layout()
+        results.append(
+            append_to_list(sum_results, sum_ix, num_res, z, []) + 
+            append_to_list(carry_results, carry_ix, len(parameters), z, [])
+        )
+
+    to_plot = list(chunks(results, 8))
+
+    sns.heatmap(to_plot, annot=True).set(title='Results of multiplication')
+    plt.savefig("multiplication_heatmap.png")
     plt.show()
-
-
-
-
-
-#print(multiply([10,0,10], [0, 10, 0]))
-
-
